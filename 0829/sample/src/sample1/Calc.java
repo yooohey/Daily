130879801598bcf196e2b8da3cf9c51e707cd0b6
yohei <yoohei.@gmail.com>
2012-08-30 00:35:00 +0900

    second commit

---
+++ 0829/sample/src/sample1/Calc.java
@@ -0,0 +1,16 @@
+package sample1;
+
+public class Calc {
+	static double rate;
+
+	Calc(double x){
+		rate = x;
+	}
+	private int weight = 100;
+	private int calc(int x){
+		return (int)(x * rate) - weight;
+	}
+	void printCalc(int x){
+		System.out.println(x + "→" + calc(x));
+	}
+}
